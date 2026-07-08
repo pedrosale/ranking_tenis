7c84cdcf5431e3d160142f38a21127125be1ffe2
pedrosale <53319245+pedrosale@users.noreply.github.com>
2026-07-08 09:23:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+const CACHE_NAME = "segunda-sagrada-v1";
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(["./", "./index.html", "./manifest.json"])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
