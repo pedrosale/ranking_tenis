6a4fb87098eb35028325f503816765741c1ccd96
pedrosale <53319245+pedrosale@users.noreply.github.com>
2026-08-24 16:46:45 -0300

    feat: route installed app root to master

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "segunda-sagrada-v4";
-const APP_SHELL = ["./", "./index.html", "./instalar.html", "./manifest.json"];
+const CACHE_NAME = "segunda-sagrada-v5";
+const APP_SHELL = ["./", "./index.html", "./master.html", "./duplas.html", "./instalar.html", "./manifest.json"];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
@@ -23,6 +23,25 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+
+  const requestUrl = new URL(event.request.url);
+  const scopeUrl = new URL("./", self.location.href);
+  const isAppRootNavigation = event.request.mode === "navigate" && requestUrl.pathname === scopeUrl.pathname;
+
+  if (isAppRootNavigation) {
+    event.respondWith(
+      fetch("./master.html", { cache: "no-store" })
+        .then(response => {
+          if (response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put("./master.html", copy));
+          }
+          return response;
+        })
+        .catch(() => caches.match("./master.html"))
+    );
+    return;
+  }
 
   event.respondWith(
     fetch(event.request)
